8b99c5bc1e0d8c6d975cec927742730e747fe2ca
sbd-Quartic <sbd.quartic@gmail.com>
2013-02-06 14:00:47 +0000

    Cleaning

--- MurmurHash64.h
+++
@@ -1,9 +0,0 @@
-#ifndef MURMURHASH_H
-#define MURMURHASH_H
-
-#include <stdint.h>
-uint64_t MurmurHash64A ( const void * key, int len, unsigned int seed ); 
-uint64_t MurmurHash64B ( const void * key, int len, unsigned int seed );
-#include "MurmurHash64.c"
-
-#endif /* MURMURHASH_H */
